8f1f15f5a3779d4d3bf977a4898843ef3598307c
Samuel Male <samuelsmalek@gmail.com>
2022-05-04 19:19:36 +0300

    Fix launch Intake A form

--- src/forms/forms-registry.ts
+++ src/forms/forms-registry.ts
@@ -2,7 +2,7 @@ import intake_a_1_0 from "./intake-a/1.0.json";
 import intake_b_1_0 from "./intake-b/1.0.json";
 
 export default {
-  hiv: {
+  "eth-hiv": {
     intake_a: {
       "1.0": intake_a_1_0,
     },
