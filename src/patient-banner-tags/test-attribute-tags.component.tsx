717c1aa96815d8c6b9513d5b297040b560013e82
sinteco <centg2g@gmail.com>
2023-03-21 09:36:04 +0300

    fix lint issue

--- src/patient-banner-tags/test-attribute-tags.component.tsx
+++ src/patient-banner-tags/test-attribute-tags.component.tsx
@@ -26,8 +26,6 @@ const TestAttributeTags: React.FC<TestAttributeTagsProps> = ({
       (x) => (x.uuid = "647db07d-8be5-4466-92a0-a7c46db589ba")
     );
     setpatientTag(HIVrelatedopportunisticinfections.display);
-    console.log(HIVrelatedopportunisticinfections.display);
-    console.log("d is a latter");
   });
 
   return patientTag != "Loading" ? <Tag type="gray">{patientTag}</Tag> : null;
