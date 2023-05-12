6127edcdbba32d6237c0758bad835e5f49675ea6
pirupius <piruville@gmail.com>
2023-05-12 11:10:32 +0300

    house keeping

--- src/ethiohri-dashboard.meta.tsx
+++ src/ethiohri-dashboard.meta.tsx
@@ -1,10 +1,3 @@
-// Template for adding new sidenav links
-// export const ethiohriDashboardMeta = {
-//   slot: "ethiohri-dashboard-slot",
-//   config: { columns: 1, type: "grid" },
-//   title: "Ethiohri Dashboard",
-// };
-
 export const PMTCT_META = {
   title: "PMTCT",
   slotName: "ethio-pmtct-slot",
