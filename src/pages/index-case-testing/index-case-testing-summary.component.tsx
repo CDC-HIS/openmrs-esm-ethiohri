14035bda80b0b75d7c97137dc31f31a83a22207c
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2023-08-25 14:26:32 +0300

    ICT Components

---
+++ src/pages/index-case-testing/index-case-testing-summary.component.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import { Tab, Tabs, TabList, TabPanels, TabPanel } from "@carbon/react";
+import React from "react";
+import styles from "../program-management/program-management.scss";
+import ICTGeneral from "./tabs/ict-general.component";
+import IndexContactFollowup from "./tabs/index-contact-followup.component";
+
+const IndexCaseTesting: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
+  return (
+    <div className={styles.tabContainer}>
+      <Tabs>
+        <TabList contained>
+          <Tab>ICT General</Tab>
+          <Tab>Index Contact Followup</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <ICTGeneral patientUuid={patientUuid} />
+          </TabPanel>
+          <TabPanel>
+            <IndexContactFollowup patientUuid={patientUuid} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+};
+
+export default IndexCaseTesting;
---
+++ src/pages/index-case-testing/tabs/index-contact-followup.component.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { EmptyStateComingSoon } from "@ohri/openmrs-esm-ohri-commons-lib";
+
+const IndexContactFollowup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
+  const pageTitle = "Index Contact Followup Summary";
+
+  return (
+    <EmptyStateComingSoon displayText={pageTitle} headerTitle={pageTitle} />
+  );
+};
+
+export default IndexContactFollowup;
