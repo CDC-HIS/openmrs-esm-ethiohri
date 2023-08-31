f756f76da4771412653dbd5edee69d84da9c1538
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2023-08-31 15:58:55 +0300

    ICT general, offer and followup tab contents

--- src/pages/index-case-testing/tabs/ict-general.component.tsx
+++ src/pages/index-case-testing/tabs/ict-general.component.tsx
@@ -1,12 +1,83 @@
-/* eslint-disable prettier/prettier */
 import React from "react";
-import { EmptyStateComingSoon } from "@ohri/openmrs-esm-ohri-commons-lib";
+import { EncounterList } from "@ohri/openmrs-esm-ohri-commons-lib";
+import { ICT_GENERAL_ENCOUNTER_TYPE } from "../../../constants";
+import { getData } from "../../encounterUtils";
+import { moduleName } from "../../../index";
 
-const ICTGeneral: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
-  const pageTitle = "ICT General Summary";
+const columns = [
+  {
+    key: "ictNumber",
+    header: "ICT Serial Number",
+    getValue: (encounter) => {
+      return getData(encounter, "b35f9632-9ff8-410f-bfcb-f497023bbcf9", true);
+    },
+  },
+  {
+    key: "indexFirstName",
+    header: "First Name",
+    getValue: (encounter) => {
+      return getData(encounter, "166102AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    },
+  },
+  {
+    key: "indexLastName",
+    header: "Last Name",
+    getValue: (encounter) => {
+      return getData(encounter, "166103AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    },
+  },
+  {
+    key: "entryPoint",
+    header: "Entry Point",
+    getValue: (encounter) => {
+      return getData(encounter, "1201b688-45f8-4e56-b089-0b31138a19dd");
+    },
+  },
+  {
+    key: "targetPopulation",
+    header: "Target Population",
+    getValue: (encounter) => {
+      return getData(encounter, "ca2c04ba-d9bd-4bad-ab03-e57ea9e49016");
+    },
+  },
+  {
+    key: "actions",
+    header: "Actions",
+    getValue: (encounter) => [
+      {
+        form: { name: "ICT General", package: "eth_hiv" },
+        encounterUuid: encounter.uuid,
+        intent: "*",
+        label: "View ICT General",
+        mode: "view",
+      },
+      {
+        form: { name: "ICT General", package: "eth_hiv" },
+        encounterUuid: encounter.uuid,
+        intent: "*",
+        label: "Edit ICT General",
+        mode: "edit",
+      },
+    ],
+  },
+];
 
+const ICTGeneral: React.FC<{ patientUuid: string }> = ({
+  patientUuid,
+}) => {
   return (
-    <EmptyStateComingSoon displayText={pageTitle} headerTitle={pageTitle} />
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterType={ICT_GENERAL_ENCOUNTER_TYPE}
+      formList={[{ name: "ICT General" }]}
+      columns={columns}
+      description="ICT General List"
+      headerTitle="ICT General"
+      launchOptions={{
+        displayText: "Add",
+        moduleName: moduleName,
+      }}
+    />
   );
 };
 
--- src/pages/index-case-testing/tabs/index-contact-followup.component.tsx
+++ src/pages/index-case-testing/tabs/index-contact-followup.component.tsx
@@ -1,12 +1,76 @@
-/* eslint-disable prettier/prettier */
 import React from "react";
-import { EmptyStateComingSoon } from "@ohri/openmrs-esm-ohri-commons-lib";
+import { EncounterList } from "@ohri/openmrs-esm-ohri-commons-lib";
+import { INDEX_CONTACT_FOLLOWUP_ENCOUNTER_TYPE } from "../../../constants";
+import { getData } from "../../encounterUtils";
+import { moduleName } from "../../../index";
 
-const IndexContactFollowup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
-  const pageTitle = "Index Contact Followup Summary";
+const columns = [
+  {
+    key: "contactFirstName",
+    header: "Contact First Name",
+    getValue: (encounter) => {
+      return getData(encounter, "166102AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", true);
+    },
+  },
+  {
+    key: "contactLastName",
+    header: "Contact Last Name",
+    getValue: (encounter) => {
+      return getData(encounter, "166103AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    },
+  },
+  {
+    key: "contactCategory",
+    header: "Contact Category",
+    getValue: (encounter) => {
+      return getData(encounter, "a7d17e7a-a8b2-49b5-84f1-aade1277b658");
+    },
+  },
+  {
+    key: "contactTrialsOutcome",
+    header: "Contact Trial Outcome",
+    getValue: (encounter) => {
+      return getData(encounter, "da8e65a1-04ee-44a0-be4b-d2bc4f002aa4");
+    },
+  },
+  {
+    key: "actions",
+    header: "Actions",
+    getValue: (encounter) => [
+      {
+        form: { name: "Index Contact Followup", package: "eth_hiv" },
+        encounterUuid: encounter.uuid,
+        intent: "*",
+        label: "View Index Contact Followup",
+        mode: "view",
+      },
+      {
+        form: { name: "Index Contact Followup", package: "eth_hiv" },
+        encounterUuid: encounter.uuid,
+        intent: "*",
+        label: "Edit Index Contact Followup",
+        mode: "edit",
+      },
+    ],
+  },
+];
 
+const IndexContactFollowup: React.FC<{ patientUuid: string }> = ({
+  patientUuid,
+}) => {
   return (
-    <EmptyStateComingSoon displayText={pageTitle} headerTitle={pageTitle} />
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterType={INDEX_CONTACT_FOLLOWUP_ENCOUNTER_TYPE}
+      formList={[{ name: "Index Contact Followup" }]}
+      columns={columns}
+      description="Index Contact Followup List"
+      headerTitle="Index Contact Followup"
+      launchOptions={{
+        displayText: "Add",
+        moduleName: moduleName,
+      }}
+    />
   );
 };
 
