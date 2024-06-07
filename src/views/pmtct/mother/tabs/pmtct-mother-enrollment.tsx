bb2a71b25e8a4b9bb3c39b90d40acf5a60c678d6
aman-icap <atg2156@cumc.columbia.edu>
2024-06-07 10:45:41 +0300

    (chore) block PMTCT Maternal forms if Intake A or Followup forms has not been filled

--- src/views/pmtct/mother/tabs/pmtct-mother-enrollment.tsx
+++ src/views/pmtct/mother/tabs/pmtct-mother-enrollment.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { EncounterList } from "@ohri/openmrs-esm-ohri-commons-lib";
-import { PMTCT_MOTHER_ENROLLMENT_ENCOUNTER_TYPE } from "../../../../constants";
-import { getData } from "../../../encounterUtils";
+import {
+  FOLLOWUP_ENCOUNTER_TYPE,
+  INTAKE_A_ENCOUNTER_TYPE,
+  PMTCT_MOTHER_ENROLLMENT_ENCOUNTER_TYPE,
+  formWarning,
+} from "../../../../constants";
+import { doesEncounterExist, getData } from "../../../encounterUtils";
 import { moduleName } from "../../../../index";
+import styles from "../../../../root.scss";
 
 const columns = [
   {
@@ -51,6 +57,24 @@ const columns = [
 const PMTCTMotherEnrollmentEncounterList: React.FC<{ patientUuid: string }> = ({
   patientUuid,
 }) => {
+  const [hasIntakeAEncounter, setHasIntakeAEncounter] = useState(false);
+  const [hasFollowupEncounter, setHasFollowupEncounter] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await doesEncounterExist(
+        patientUuid,
+        INTAKE_A_ENCOUNTER_TYPE,
+        setHasIntakeAEncounter
+      );
+      await doesEncounterExist(
+        patientUuid,
+        FOLLOWUP_ENCOUNTER_TYPE,
+        setHasFollowupEncounter
+      );
+    })();
+  });
+
   return (
     <>
       <EncounterList
@@ -63,8 +87,15 @@ const PMTCTMotherEnrollmentEncounterList: React.FC<{ patientUuid: string }> = ({
         launchOptions={{
           displayText: "Add",
           moduleName: moduleName,
+          hideFormLauncher: !hasIntakeAEncounter || !hasFollowupEncounter,
         }}
       />
+      {!hasIntakeAEncounter && (
+        <p className={styles.patientName}>{formWarning("Intake A")}</p>
+      )}
+      {!hasFollowupEncounter && (
+        <p className={styles.patientName}>{formWarning("Followup")}</p>
+      )}
     </>
   );
 };
