be5d65c942eaf2e95dab3b8ac8bfca344ac9f397
aman-icap <atg2156@cumc.columbia.edu>
2024-06-13 09:47:11 +0300

    (fix) fix viral load status calculation

--- src/custom-expressions.ts
+++ src/custom-expressions.ts
@@ -98,16 +98,14 @@ export function CalcMonthsOnART(artStartDate: Date) {
 export function CalcViralLoadStatus(viralLoadCount: number) {
   let resultViralLoadStatus: string;
   if (viralLoadCount) {
-    if (viralLoadCount == 0) {
+    if (viralLoadCount === 0) {
       resultViralLoadStatus = "167484AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else if (viralLoadCount <= 50) {
-      resultViralLoadStatus = "167378AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+      resultViralLoadStatus = "167484AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else if (viralLoadCount >= 51 && viralLoadCount <= 1000) {
       resultViralLoadStatus = "167378AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else if (viralLoadCount > 1000) {
       resultViralLoadStatus = "162185AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    } else {
-      resultViralLoadStatus = "167485AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     }
   }
   return viralLoadCount ? resultViralLoadStatus : null;
