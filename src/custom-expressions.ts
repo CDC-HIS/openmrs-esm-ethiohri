7f075abf4c49ee694d56743a97acf3c940fb5cb9
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2024-07-12 11:16:55 +0300

    Add one option for "obese" on nutritional status and screening when BMI is >= 30

--- src/custom-expressions.ts
+++ src/custom-expressions.ts
@@ -176,6 +176,8 @@ export function CalcAdultNutritionalStatus(
       nutritionalStatus = "126598AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else if (resultBMI >= 25 && resultBMI <= 29.99) {
       nutritionalStatus = "114413AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    } else if (resultBMI >= 30) {
+      nutritionalStatus = "132626AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else {
       return null;
     }
@@ -206,6 +208,8 @@ export function CalcNutritionalScreening(
       nutritionalScreening = "123815AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else if (resultBMI >= 25 && resultBMI <= 29.99) {
       nutritionalScreening = "114413AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    } else if (resultBMI >= 30) {
+      nutritionalScreening = "132626AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     } else {
       return null;
     }
