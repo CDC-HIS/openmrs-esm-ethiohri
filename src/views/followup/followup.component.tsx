ce7830203abc8204af4706ac4fb19d2d80543fec
BisratB-ICAP <bisrat.birhanu@gmail.com>
2025-06-13 14:25:33 +0300

    (fix) hide followup warning message until loading is complete

--- src/views/followup/followup.component.tsx
+++ src/views/followup/followup.component.tsx
@@ -13,7 +13,12 @@ import {
 import { getData } from "../encounterUtils";
 import { moduleName } from "../../index";
 import styles from "./followup.scss";
-import { fetchIdentifiers, getLatestObs, getPatientEncounters } from "../../api/api";
+import {
+  fetchIdentifiers,
+  getLatestObs,
+  getPatientEncounters,
+} from "../../api/api";
+import { InlineLoading } from "@carbon/react";
 
 const Followup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
   const columns: EncounterListColumn[] = useMemo(
@@ -25,51 +30,29 @@ const Followup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
           const rawDate = getData(encounter, "5c118396-52dc-4cac-8860-e6d8e4a7f296", true);
           return rawDate ? rawDate.split(',')[0].trim() : "";
         },
-      }, 
+      },
       {
         key: "artRegimen",
         header: "ART Regimen",
-        getValue: (encounter) => {
-          return getData(encounter, "6d7d0327-e1f8-4246-bfe5-be1e82d94b14");
-        },
+        getValue: (encounter) => getData(encounter, "6d7d0327-e1f8-4246-bfe5-be1e82d94b14"),
       },
       {
         key: "followUpStatus",
         header: "Follow Up Status",
-        // getValue: (encounter) => {
-        //   return getData(encounter, "222f64a8-a603-4d2e-b70e-2d90b622bb04");
-        // },
         getValue: (encounter) => {
-          const status = getData(
-            encounter,
-            "222f64a8-a603-4d2e-b70e-2d90b622bb04"
-          );
-          if (status === "Restart medication") {
-            return "Restart";
-          } else if (status === "Ran away") {
-            return "Drop";
-          } else if (status === "Stop all") {
-            return "Stop";
-          } else if (status === "Loss to follow-up (LTFU)") {
-            return "Lost";
-          } 
-          return status; 
+          const status = getData(encounter, "222f64a8-a603-4d2e-b70e-2d90b622bb04");
+          if (status === "Restart medication") return "Restart";
+          if (status === "Ran away") return "Drop";
+          if (status === "Stop all") return "Stop";
+          if (status === "Loss to follow-up (LTFU)") return "Lost";
+          return status;
         },
       },
       {
         key: "weight",
         header: "Weight",
-        getValue: (encounter) => {
-          return getData(encounter, "4ab93a3c-4373-4b9b-9268-5ff0641cc242");
-        },
+        getValue: (encounter) => getData(encounter, "4ab93a3c-4373-4b9b-9268-5ff0641cc242"),
       },
-      // {
-      //   key: "viralLoadCount",
-      //   header: "Viral Load Count",
-      //   getValue: (encounter) => {
-      //     return getData(encounter, "856AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-      //   },
-      // },
       {
         key: "dateViralLoadRequested",
         header: "VL Sent Date",
@@ -81,58 +64,31 @@ const Followup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
       {
         key: "vlStatus",
         header: "VL Status",
-        // getValue: (encounter) => {
-        //   return getData(encounter, "2dc9ee04-4d12-4606-ae0f-86895bf14a44");
-        // },
         getValue: (encounter) => {
-          const vlStatus = getData(
-            encounter,
-            "2dc9ee04-4d12-4606-ae0f-86895bf14a44"
-          );
-          if (vlStatus === "LLV") {
-            return "Low Level Viremia";
-          } else if (vlStatus === "HIV infection with high viral load") {
-            return "High Viral Load";
-          }
-          return vlStatus; 
+          const vlStatus = getData(encounter, "2dc9ee04-4d12-4606-ae0f-86895bf14a44");
+          if (vlStatus === "LLV") return "Low Level Viremia";
+          if (vlStatus === "HIV infection with high viral load") return "High Viral Load";
+          return vlStatus;
         },
       },
       {
         key: "dsdCategory",
         header: "DSD Category",
-        // getValue: (encounter) => {
-        //   return getData(encounter, "defeb4ff-d07b-4e4a-bbd6-d4281c1384a2");
-        // },
         getValue: (encounter) => {
-          const category = getData(
-            encounter,
-            "defeb4ff-d07b-4e4a-bbd6-d4281c1384a2"
-          );
-          if (category === "Community based group model by peer") {
-            return "PCAD";
-          } else if (category === "AHID") {
-            return "AHD";
-          } 
-          return category; 
+          const category = getData(encounter, "defeb4ff-d07b-4e4a-bbd6-d4281c1384a2");
+          if (category === "Community based group model by peer") return "PCAD";
+          if (category === "AHID") return "AHD";
+          return category;
         },
       },
       {
         key: "screeningDone",
         header: "CXCA Screening Done",
-        // getValue: (encounter) => {
-        //   return getData(encounter, "01c546b4-e08a-4c0c-82ef-d387cab6bbbf");
-        // },
         getValue: (encounter) => {
-          const screened = getData(
-            encounter,
-            "01c546b4-e08a-4c0c-82ef-d387cab6bbbf"
-          );
-          if (screened === "Cervical cancer screening not performed") {
-            return "No";
-          } else if (screened === "Cervical cancer screening performed") {
-            return "Yes";
-          }
-          return screened; 
+          const screened = getData(encounter, "01c546b4-e08a-4c0c-82ef-d387cab6bbbf");
+          if (screened === "Cervical cancer screening not performed") return "No";
+          if (screened === "Cervical cancer screening performed") return "Yes";
+          return screened;
         },
       },
       {
@@ -179,50 +135,41 @@ const Followup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
   const [hasIntakeAEncounter, setHasIntakeAEncounter] = useState(false);
   const [isDead, setIsDead] = useState(false);
   const [isFormSaved, setIsFormSaved] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const updateFormSavedStatus = useCallback(() => {
-      setIsFormSaved((prev) => !prev);
-    }, []);
+    setIsFormSaved((prev) => !prev);
+  }, []);
 
   useEffect(() => {
     (async () => {
-      const identifiers = await fetchIdentifiers(patientUuid);
-      if (identifiers?.find((e) => e.identifierType.display === "MRN")) {
-        setHasMRN(true);
-      }
-    })();
+      const [identifiers, encounters, latestObs] = await Promise.all([
+        fetchIdentifiers(patientUuid),
+        getPatientEncounters(patientUuid, INTAKE_A_ENCOUNTER_TYPE),
+        getLatestObs(
+          patientUuid,
+          "222f64a8-a603-4d2e-b70e-2d90b622bb04", // Follow-up status
+          FOLLOWUP_ENCOUNTER_TYPE
+        ),
+      ]);
 
-    (async () => {
-      const identifiers = await fetchIdentifiers(patientUuid);
-      if (identifiers?.find((e) => e.identifierType.display === "UAN")) {
-        setHasUAN(true);
-      }
+      setHasMRN(identifiers?.some((e) => e.identifierType.display === "MRN"));
+      setHasUAN(identifiers?.some((e) => e.identifierType.display === "UAN"));
+      setHasIntakeAEncounter(encounters.length > 0);
+      setIsDead(
+        latestObs?.valueCodeableConcept?.text?.toLowerCase() === "dead"
+      );
+      setIsLoading(false);
     })();
+  }, [patientUuid]);
 
-    (async () => {
-      const previousEncounters = await getPatientEncounters(
-        patientUuid,
-        INTAKE_A_ENCOUNTER_TYPE
-      );
-      if (previousEncounters.length) {
-        setHasIntakeAEncounter(true);
-      }
-    })();
-    (async () => {
-      const latestObs = await getLatestObs(
-        patientUuid,
-        "222f64a8-a603-4d2e-b70e-2d90b622bb04", // Follow-up status
-        FOLLOWUP_ENCOUNTER_TYPE
-      );
-    
-      if (latestObs?.valueCodeableConcept?.text?.toLowerCase() === "dead") {
-        setIsDead(true);
-      } else {
-        setIsDead(false);
-      }
-    })();
-    
-  });
+  if (isLoading)
+  return (
+    <div className={styles.loadingContainer}>
+      <InlineLoading description="Loading ..." />
+    </div>
+  );
+
   return (
     <>
       {!hasMRN ? (
@@ -231,7 +178,8 @@ const Followup: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
         <p className={styles.warningMessage}>{formWarning("Intake A")}</p>
       ) : isDead ? (
         <p className={styles.warningMessage}>
-          ⚠️ Patient last follow-up status is set to be Dead, please edit the previous follow-up before preceeding.
+          ⚠️ Patient last follow-up status is set to be Dead, please edit the
+          previous follow-up before proceeding.
         </p>
       ) : !hasUAN ? (
         <p className={styles.warningMessage}>{UAN_NULL_WARNING}</p>
