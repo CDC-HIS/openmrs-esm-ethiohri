3cb9fd18c3f1155c79fbdc2dadfdeee082800668
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2025-07-14 09:08:30 +0300

    feat: migrate encounter lists from custom components to config-driven approach

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
-/* eslint-disable unused-imports/no-unused-imports */
-import {
-  getAsyncLifecycle,
-  getSyncLifecycle,
-  defineConfigSchema,
-} from "@openmrs/esm-framework";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getAsyncLifecycle, getSyncLifecycle } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
 import {
   registerControl,
   registerExpressionHelper,
 } from "@openmrs/esm-form-engine-lib";
-import {
-  createDashboardGroup,
-  createDashboardLink,
-} from "@openmrs/esm-patient-common-lib";
-import {
-  CHILD_HEALTH_SUMMARY,
-  CLINICAL_VISITS,
-  HIV_CARE_AND_TREATMENT,
-  HIV_TESTING_SERVICE_META,
-  INDEX_CASE_TESTING_META,
-  PMTCT_META,
-  POST_META,
-  PREP_META,
-  PROGRAM_MANAGEMENT_META,
-  MOTHER_HEALTH_SUMMARY,
-} from "./ethiohri-dashboard.meta";
 import {
   CalcNextVisitDate,
   CalcTreatmentEndDate,
@@ -52,21 +32,9 @@ import {
   getAgeFromBirthdate,
   getEligibilityStatus,
 } from "./custom-expressions";
-import {
-  createConditionalDashboardGroup,
-  createConditionalDashboardLink,
-} from "@ohri/openmrs-esm-ohri-commons-lib";
-import ProgramManagment from "./views/program-management/program-managment-summary.component";
-import VisitsSummary from "./views/followup/followup.component";
-import MotherHealth from "./views/pmtct/mother/pmtct-mother.component";
-import PreExposure from "./views/pre-exposure/pre-exposure-summary.component";
-import PostExposure from "./views/post-exposure/post-exposure.component";
-import HIVTestingService from "./views/hiv-testing-service/hiv-testing-service-summary.component";
-import IndexCaseTesting from "./views/index-case-testing/index-case-testing-summary.component";
 import ActiveMedications from "./views/medications/active-medications.component";
 import VitalsSummary from "./views/vitals/vitals-summary.component";
 import HivBaselineSummary from "./views/hiv-baseline/hiv-baseline-summary.component";
-import ChildHealth from "./views/pmtct/child/hei.component";
 import { PatientList } from "./components/patient-lists/patient-list.component";
 import { getValidPatientFollowUpDate } from "./api/api";
 
@@ -80,7 +48,6 @@ export const importTranslation = require.context(
 );
 
 export function startupApp() {
-  defineConfigSchema(moduleName, configSchema);
   registerExpressionHelper("CustomNextVisitDateCalc", CalcNextVisitDate);
   registerExpressionHelper("CustomTreatmentEndDateCalc", CalcTreatmentEndDate);
   registerExpressionHelper("CustomPrepDoseEndDateCalc", CalcPrepDoseEndDate);
@@ -161,91 +128,8 @@ export const ethiohriActiveMedications = getSyncLifecycle(
 );
 export const vitalsOverview = getSyncLifecycle(VitalsSummary, options);
 
-export const hivCareAndTreatmentMenu = getSyncLifecycle(
-  createDashboardGroup(HIV_CARE_AND_TREATMENT),
-  options
-);
 export const facilityName = getAsyncLifecycle(
   () => import("./views/navbar/facility-name.component"),
-  options
-);
-export const programManagementMenu = getSyncLifecycle(
-  createDashboardLink({
-    ...PROGRAM_MANAGEMENT_META,
-    moduleName,
-  }),
-  options
-);
-
-export const programManagementChart = getSyncLifecycle(
-  ProgramManagment,
-  options
-);
-export const clinicalVisitsMenu = getSyncLifecycle(
-  createDashboardLink({
-    ...CLINICAL_VISITS,
-    moduleName,
-  }),
-  options
-);
-export const clinicalVisitsChart = getSyncLifecycle(VisitsSummary, options);
-
-export const pmtctMenu = getSyncLifecycle(
-  createConditionalDashboardGroup(PMTCT_META),
-  options
-);
-export const childHealthMenu = getSyncLifecycle(
-  createConditionalDashboardLink({
-    ...CHILD_HEALTH_SUMMARY,
-    moduleName,
-  }),
-  options
-);
-export const childHealthChart = getSyncLifecycle(ChildHealth, options);
-export const motherHealthMenu = getSyncLifecycle(
-  createConditionalDashboardLink({
-    ...MOTHER_HEALTH_SUMMARY,
-    moduleName,
-  }),
-  options
-);
-export const motherHealthChart = getSyncLifecycle(MotherHealth, options);
-export const prepMenu = getSyncLifecycle(
-  createDashboardLink({
-    ...PREP_META,
-    moduleName,
-  }),
-  options
-);
-export const prepChart = getSyncLifecycle(PreExposure, options);
-export const pepMenu = getSyncLifecycle(
-  createDashboardLink({
-    ...POST_META,
-    moduleName,
-  }),
-  options
-);
-export const pepChart = getSyncLifecycle(PostExposure, options);
-export const hivTestingServiceMenu = getSyncLifecycle(
-  createDashboardLink({
-    ...HIV_TESTING_SERVICE_META,
-    moduleName,
-  }),
-  options
-);
-export const hivTestingServiceChart = getSyncLifecycle(
-  HIVTestingService,
-  options
-);
-export const indexCaseTestingMenu = getSyncLifecycle(
-  createDashboardLink({
-    ...INDEX_CASE_TESTING_META,
-    moduleName,
-  }),
-  options
-);
-export const indexCaseTestingChart = getSyncLifecycle(
-  IndexCaseTesting,
   options
 );
 
