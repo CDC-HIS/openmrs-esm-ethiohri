58481fa4e73229f13a1d6464ad062125bde0dd7b
BisratB-ICAP <bisrat.birhanu@gmail.com>
2025-07-22 14:02:16 +0300

    (fix) resolve older child nutritional status custom expression calc

--- src/custom-expressions.ts
+++ src/custom-expressions.ts
@@ -309,21 +309,23 @@ export function CalcAdultNutritionalStatus(height, weight, muac, functionalStatu
 
 
 export function CalcOlderChildNutritionalStatus(bmiForAge) {
-  switch (bmiForAge) {
-    case "c93ec1cc-a4eb-43b9-b99b-ace42ca6106f":
-      return "1115AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    case "6f384ab3-5587-478e-a685-0b43c0f64163":
-      return "134723AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    case "b782c7a5-639e-4f7e-9eee-608a62439885":
-      return "134722AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    case "c3354c3c-b708-4821-94ee-cebc9eadf1e3":
-      return "126598AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    case "9324e838-c96d-4312-91b0-deae5cc0334c":
-      return "114413AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    case "9a41b3bb-7c37-40f2-9022-d0f672e171cc":
-      return "114413AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    default:
-      return 0;
+  if (bmiForAge) {    
+      switch (bmiForAge) {
+      case "c93ec1cc-a4eb-43b9-b99b-ace42ca6106f":          // Z Score: >-1 and <+2
+        return "1115AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";   // Normal
+      case "6f384ab3-5587-478e-a685-0b43c0f64163":          // Z Score: <-1 and >-2
+        return "134723AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";   // Mild Malnutrition
+      case "b782c7a5-639e-4f7e-9eee-608a62439885":         // Z Score: <-2 and >-3
+        return "134722AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";   // Moderate Malnutrition
+      case "c3354c3c-b708-4821-94ee-cebc9eadf1e3":        // Z Score: <-3
+        return "126598AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";   // Severe Malnutrition
+      case "9324e838-c96d-4312-91b0-deae5cc0334c":        // Z Score: >+3
+        return "114413AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";   // Overweight
+      case "9a41b3bb-7c37-40f2-9022-d0f672e171cc":        // Z Score: >+2 and <+3
+        return "114413AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";   // Overweight
+      default:
+        return null;
+    }
   }
 }
 
