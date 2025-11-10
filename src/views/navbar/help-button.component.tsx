9f225aa44a333fc9adb536e45e7738b6074fd6f6
yeshewas <yeshewasg12@gmail.com>
2025-11-10 17:22:36 +0300

    help new version

--- src/views/navbar/help-button.component.tsx
+++ src/views/navbar/help-button.component.tsx
@@ -6,7 +6,7 @@ import styles from "./navbar.scss";
 const HelpMenu: React.FC = () => {
   const currentUrl = `${window.location.protocol}//${window.location.hostname}${
     window.location.port ? `:${window.location.port}` : ""
-  }/openmrs/spa/EthiOHRI_1.5_Training_Participant_Manual.pdf`;
+  }/openmrs/spa/EthiOHRI_V1.6_User_Guide.pdf.pdf`;
 
   return (
     <div className={styles.switcherContainer}>
