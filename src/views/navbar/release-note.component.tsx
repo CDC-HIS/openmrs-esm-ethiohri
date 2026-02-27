c2eab8666cf5c9bd0d2bd963cd3fe29818660248
BisratB-ICAP <bisrat.birhanu@gmail.com>
2026-02-27 14:39:50 +0300

    update release note

--- src/views/navbar/release-note.component.tsx
+++ src/views/navbar/release-note.component.tsx
@@ -6,7 +6,7 @@ import styles from "./navbar.scss";
 const ReleaseNoteMenu: React.FC = () => {
   const currentUrl = `${window.location.protocol}//${window.location.hostname}${
     window.location.port ? `:${window.location.port}` : ""
-  }/openmrs/spa/Ethiohri_1.6_Release_Note.pdf`;
+  }/openmrs/spa/Ethiohri_2.0_Release_Note.pdf`;
 
   return (
     <div className={styles.switcherContainer}>
